96d34de02ad836c9d47ecec6060cf6e31e5b1343
B Prashanth <113793561+preshu2103@users.noreply.github.com>
2023-05-28 14:36:45 +0530

    Add files via upload

---
+++ heart detection.py
@@ -0,0 +1,8 @@
+# Importing the required packages 
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn import tree
+import graphviz
